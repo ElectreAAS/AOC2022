46e588fd8a32e2008a9f50a91a15966f2ea5a5d8
Ambre Austen Suhamy <ambre@tarides.com>
2022-12-02 14:36:52 +0100

    Added eio for file system stuff

--- utils/utils.ml
+++ utils/utils.ml
@@ -2,18 +2,12 @@ module Types = struct
   include Types
 end
 
-let slurp filename =
-  let ic = open_in filename in
-  let lines = ref [] in
-  let rec read () =
-    let line = input_line ic in
-    lines := line :: !lines;
-    read ()
-  in
-  try read ()
-  with End_of_file ->
-    close_in ic;
-    String.concat "\n" (List.rev ("\n" :: !lines))
+let ( / ) = Eio.Path.( / )
+
+let slurp path =
+  Eio_main.run @@ fun env ->
+  let path = Eio.Stdenv.fs env / path in
+  Eio.Path.load path
 
 let get_input n = Printf.sprintf "day%d/input.txt" n |> slurp
 let get_test n = Printf.sprintf "../../../day%d/test.txt" n |> slurp
