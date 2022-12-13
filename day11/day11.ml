7d282bcb5ebe879cc626e2daff4ef13d5c387f48
Ambre Austen Suhamy <ambre@tarides.com>
2022-12-13 19:33:03 +0100

    Added part 2 of day 11

--- day11/day11.ml
+++ day11/day11.ml
@@ -16,60 +16,57 @@ let parse_list str =
   |> List.map (fun str -> String.trim str |> int_of_string)
 
 let parse_op opchar operand old =
-  let op =
-    match opchar with '+' -> ( + ) | '-' -> ( - ) | '*' -> ( * ) | _ -> ( / )
-  in
+  let op = if opchar = '+' then ( + ) else ( * ) in
   match int_of_string_opt operand with Some n -> op old n | None -> op old old
 
 let parse_test divis cons alt x = if x mod divis = 0 then cons else alt
 
 let parse contents =
   let ic = Scanf.Scanning.from_string contents in
-  let rec loop monkeys =
+  let rec loop monkeys ppcm =
     try
-      let monkey =
+      let monkey, modulo =
         Scanf.bscanf ic
           " Monkey %_d: Starting items: %s@\n\
           \ Operation: new = old %c %s Test: divisible by %d If true: throw to \
            monkey %d If false: throw to monkey %d "
           (fun items opchar operand divis cons alt ->
-            {
-              items = parse_list items;
-              operation = parse_op opchar operand;
-              test = parse_test divis cons alt;
-            })
+            ( {
+                items = parse_list items;
+                operation = parse_op opchar operand;
+                test = parse_test divis cons alt;
+              },
+              divis ))
       in
-      loop (monkey :: monkeys)
-    with End_of_file -> monkeys
+      loop (monkey :: monkeys) (ppcm * modulo)
+    with End_of_file -> (monkeys, ppcm)
   in
-  loop [] |> List.rev |> Array.of_list
+  let l, ppcm = loop [] 1 in
+  (l |> List.rev |> Array.of_list, ppcm)
+
+let round active monkeys ppcm =
+  Array.iteri
+    (fun i { items; operation; test } ->
+      let rec throw = function
+        | [] -> monkeys.(i) <- { items = []; operation; test }
+        | item :: xs ->
+            let worry = operation item mod ppcm in
+            let j = test worry in
+            let target = monkeys.(j) in
+            monkeys.(j) <- { target with items = target.items @ [ worry ] };
+            active.(i) <- active.(i) + 1;
+            throw xs
+      in
+      throw items)
+    monkeys
 
 let day display contents =
-  let monkeys = parse contents in
+  let monkeys, ppcm = parse contents in
   let len = Array.length monkeys in
   let active = Array.make len 0 in
-  let round () =
-    Array.iteri
-      (fun i { items; operation; test } ->
-        let rec throw = function
-          | [] -> ()
-          | item :: xs ->
-              let worry = operation item in
-              let worry' = worry / 3 in
-              let j = test worry' in
-              assert (j <> i);
-              let target = monkeys.(j) in
-              monkeys.(j) <- { target with items = target.items @ [ worry' ] };
-              active.(i) <- active.(i) + 1;
-              throw xs
-        in
-        throw items;
-        monkeys.(i) <- { items = []; operation; test })
-      monkeys
-  in
-  for i = 1 to 20 do
-    round ();
-    if display then (
+  for i = 1 to 10_000 do
+    round active monkeys ppcm;
+    if display && i mod 1000 = 0 then (
       Printf.printf "\nAfter round %d, monkeys are holding:\n" i;
       Array.iteri
         (fun i m ->
--- test/test11.ml
+++ test/test11.ml
@@ -3,6 +3,6 @@ let day =
   Alcotest.test_case "Test puzzle input" `Quick @@ fun () ->
   let contents = Utils.get_test 11 in
   let result = day false contents in
-  let expected = "10605" in
+  let expected = "2713310158" in
   Alcotest.(check string) "puzzle input should be solved!" expected result;
   ()
