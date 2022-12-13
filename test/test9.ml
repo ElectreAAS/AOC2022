d610bf355508cd3dc8efdf26bc94f3827a221878
Ambre Austen Suhamy <ambre@tarides.com>
2022-12-13 15:16:11 +0100

    Added more complex example for day 9 part 2

--- test/test9.ml
+++ test/test9.ml
@@ -3,6 +3,6 @@ let day =
   Alcotest.test_case "Test puzzle input" `Quick @@ fun () ->
   let contents = Utils.get_test 9 in
   let result = day false contents in
-  let expected = "13" in
+  let expected = "36" in
   Alcotest.(check string) "puzzle input should be solved!" expected result;
   ()
