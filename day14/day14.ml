eeed00f58534c532df06ca6abd68adf11f98904a
Ambre Austen Suhamy <ambre@tarides.com>
2022-12-20 16:51:22 +0100

    Added part 2 of day 14

--- day14/day14.ml
+++ day14/day14.ml
@@ -14,14 +14,14 @@ let parse (minx, maxx, maxy) line =
   List.fold_left_map pathize (minx, maxx, maxy) chunks
 
 type cell = Rock | Sand | Air
-type state = { grid : cell array array; pour : int }
+type state = { mutable grid : cell array array; mutable pour : int }
 
 let sand_pour = 500
 
 let init_state (minx, maxx, maxy) (paths : path list) =
   let width = maxx - minx + 1 in
-  let height = maxy + 1 in
-  let grid = Array.init width (fun _ -> Array.init height (Fun.const Air)) in
+  let height = maxy + 3 in
+  let grid = Array.make_matrix width height Air in
   let rec draw_path = function
     | [] | [ _ ] -> ()
     | (lx, ly) :: (rx, ry) :: rest when lx = rx ->
@@ -39,6 +39,9 @@ let init_state (minx, maxx, maxy) (paths : path list) =
         draw_path ((rx, ry) :: rest)
   in
   List.iter draw_path paths;
+  for x = 0 to width - 1 do
+    grid.(x).(maxy + 2) <- Rock
+  done;
   { grid; pour = sand_pour - minx }
 
 let pp state =
@@ -71,29 +74,59 @@ let pp state =
   for _ = 0 to width + 2 do
     print_string "─"
   done;
-  print_string "┘\n"
+  Printf.printf "┘\n%!"
 
-let loop state =
-  let rec pour_opt x y =
+let expand_size = 128
+
+let expand state is_left =
+  let width = Array.length state.grid in
+  let height = Array.length state.grid.(0) in
+  let initialize x =
+    if if is_left then x >= expand_size else x < width then
+      Array.copy state.grid.(if is_left then x - expand_size else x)
+    else Array.make height Air
+  in
+  let grid = Array.init (width + expand_size) initialize in
+  for x = 0 to width + expand_size - 1 do
+    grid.(x).(height - 1) <- Rock
+  done;
+  state.grid <- grid;
+  if is_left then (
+    state.pour <- state.pour + expand_size;
+    expand_size)
+  else width - 1
+
+let run state =
+  let rec pour_from x y path =
     match
       ( state.grid.(x - 1).(y + 1),
         state.grid.(x).(y + 1),
         state.grid.(x + 1).(y + 1) )
     with
-    | exception Invalid_argument _ -> None
-    | _, Air, _ -> pour_opt x (y + 1)
-    | Air, _, _ -> pour_opt (x - 1) (y + 1)
-    | _, _, Air -> pour_opt (x + 1) (y + 1)
-    | _ -> Some (x, y)
+    | exception Invalid_argument _ ->
+        let new_x = expand state (x = 0) in
+        let new_path =
+          if x = 0 then List.map (fun (x, y) -> (x + expand_size, y)) path
+          else path
+        in
+        pour_from new_x y new_path
+    | _, Air, _ -> pour_from x (y + 1) ((x, y) :: path)
+    | Air, _, _ -> pour_from (x - 1) (y + 1) ((x, y) :: path)
+    | _, _, Air -> pour_from (x + 1) (y + 1) ((x, y) :: path)
+    | _ -> (x, y) :: path
   in
-  let rec aux n =
-    match pour_opt state.pour 0 with
-    | Some (x, y) ->
+  let rec loop n path =
+    let x, y, xs =
+      match path with [] -> (state.pour, 0, []) | (x, y) :: xs -> (x, y, xs)
+    in
+    match pour_from x y xs with
+    | [] -> failwith "Path should never be empty after pouring"
+    | (x, 0) :: _ when x = state.pour -> n
+    | (x, y) :: xs ->
         state.grid.(x).(y) <- Sand;
-        aux (n + 1)
-    | None -> n
+        loop (n + 1) xs
   in
-  aux 0
+  loop 1 []
 
 let day display contents =
   let lines = String.trim contents |> String.split_on_char '\n' in
@@ -101,6 +134,6 @@ let day display contents =
     List.fold_left_map parse (max_int, min_int, min_int) lines
   in
   let state = init_state bounds paths in
-  let res = loop state |> string_of_int in
+  let res = run state |> string_of_int in
   if display then pp state;
   res
--- test/test14.ml
+++ test/test14.ml
@@ -3,6 +3,6 @@ let day =
   Alcotest.test_case "Test puzzle input" `Quick @@ fun () ->
   let contents = Utils.get_test 14 in
   let result = day false contents in
-  let expected = "24" in
+  let expected = "93" in
   Alcotest.(check string) "puzzle input should be solved!" expected result;
   ()
